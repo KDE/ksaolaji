ed2b221276f4137813b8eef809ada4574c3edf18
Ni Hui <shuizhuyuanluo@126.com>
2011-04-06 19:54:09 +0800

    add gplv3 copyright header.

--- cleaner_kross.h
+++ cleaner_kross.h
@@ -1,3 +1,21 @@
+/*
+ *  This file is part of KSaoLaJi, an advanced system cleaner for KDE
+ *  Copyright (C) 2011 Ni Hui <shuizhuyuanluo@126.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CLEANER_KROSS_H
 #define CLEANER_KROSS_H
 
@@ -12,10 +30,7 @@ class CleanerKross : public KSaoLaJi::Cleaner
         virtual QString uniqueName();
         virtual QString description();
         virtual QString iconName();
-
-        /** you mei you la ji ? */
         virtual bool youlaji();
-        /** sao la ji */
         virtual bool saolaji();
     private:
         Kross::Action* m_action;
